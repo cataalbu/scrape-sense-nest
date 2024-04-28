e87153d589269e3397bc87dd142039f81a254845
Catalin Albu <cataalbu1919@gmail.com>
2024-04-28 13:21:25 +0300

    feat: Add nginx conf

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-# sudo certbot -n -d $DOMAIN --nginx --agree-tos --email $DOMAIN_EMAIL
+sudo certbot -n -d $DOMAIN --nginx --agree-tos --email $DOMAIN_EMAIL
+sudo systemctl restart nginx
