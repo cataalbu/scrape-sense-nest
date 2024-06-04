7cce30e57356adbf33d1bfdaf1e6bd7d2380db9c
Catalin Albu <cataalbu1919@gmail.com>
2024-06-04 11:33:35 +0300

    Remove unused file

--- smth.ts
+++
@@ -1,8 +0,0 @@
-const mockito = require('ts-mockito');
-const aws = require('@aws-sdk/client-sqs');
-const mock = mockito.mock;
-
-const mock1 = mock(aws.SQS);
-const ins = mockito.instance(mock1);
-
-console.log(ins);
